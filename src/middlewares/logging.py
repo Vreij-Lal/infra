5c83f468beb53eea8b7bb62f1460ebef36908c9e
Vreij lal <vlal@damanmarkets.com>
2025-06-18 01:58:05 +0300

    ...

--- src/middlewares/logging.py
+++ src/middlewares/logging.py
@@ -1,16 +1,27 @@
-import time
-from starlette.middleware.base import BaseHTTPMiddleware
+from starlette.middleware.base import RequestResponseEndpoint
+from starlette.types import ASGIApp, Receive, Scope, Send
 from starlette.requests import Request
+from starlette.responses import Response
 from src.logger import logger
+import time
 
-class LoggingMiddleware(BaseHTTPMiddleware):
-    async def dispatch(self, request: Request, call_next):
+class LoggingMiddleware:
+    def __init__(self, app: ASGIApp):
+        self.app = app
+
+    async def __call__(self, scope: Scope, receive: Receive, send: Send):
+        if scope["type"] != "http":
+            await self.app(scope, receive, send)
+            return
+
+        request = Request(scope, receive)
         logger.info(f"📥 Request received: {request.method} {request.url}")
         start_time = time.time()
 
-        response = await call_next(request)
+        async def send_wrapper(message):
+            if message["type"] == "http.response.start":
+                duration = time.time() - start_time
+                logger.info(f"📤 Response sent: {message['status']} ({duration:.4f}s)")
+            await send(message)
 
-        duration = time.time() - start_time
-        logger.info(f"📤 Response sent: {response.status_code} ({duration:.4f}s)")
-
-        return response
+        await self.app(scope, receive, send_wrapper)
