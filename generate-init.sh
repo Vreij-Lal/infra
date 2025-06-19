2e5fd3177bf80775b383feb713bffa9ad82944c2
Vreij lal <vlal@damanmarkets.com>
2025-06-19 18:39:55 +0300

    .

--- generate-init.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-set -e
-
-echo "🔧 Generating init.sql from template..."
-envsubst < /docker-entrypoint-initdb.d/mysql-init-template.sql > /docker-entrypoint-initdb.d/init.sql
-echo "✅ init.sql generated."
